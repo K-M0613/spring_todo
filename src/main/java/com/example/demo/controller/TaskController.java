575fadb4c6a0b7955870dd0ede92dba4d7e05231
K-M0613 <106648206+K-M0613@users.noreply.github.com>
2025-04-01 12:34:21 +0900

    Update TaskController.java

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -3,12 +3,21 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.model;
+import com.example.demo.entity.Task;
 
 import com.example.demo.service.TaskService;
 
 @Controller
-@RequestMapping("/tasks")
+@RequestMapping("/blogs")
 public class TaskController {
 	@Autowired
 	TaskService taskService;
+
+	@GetMapping
+	public String listTasks(Model model){
+		List<Task> tasks = taskService.getAllTasks();
+		model.addattibute('tasks', tasks);
+	}
 }
